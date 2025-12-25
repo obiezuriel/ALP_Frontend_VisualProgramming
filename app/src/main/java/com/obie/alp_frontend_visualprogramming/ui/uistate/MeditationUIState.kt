e55d257e375a349d184793383f8a7d6087324f97
Obie Zuriel <obiezuriel631@gmail.com>
2025-12-25 16:07:46 +0700

    Change object to interface

--- app/src/main/java/com/obie/alp_frontend_visualprogramming/ui/uistate/MeditationUIState.kt
+++ app/src/main/java/com/obie/alp_frontend_visualprogramming/ui/uistate/MeditationUIState.kt
@@ -2,9 +2,9 @@ package com.obie.alp_frontend_visualprogramming.ui.uistate
 
 import com.obie.alp_frontend_visualprogramming.ui.model.MeditationWrapper
 
-sealed class MeditationUIState {
-    object Start : MeditationUIState()
-    object Loading : MeditationUIState()
-    data class Success(val meditations: List<MeditationWrapper.MeditationData>) : MeditationUIState()
-    data class Error(val message: String) : MeditationUIState()
+sealed interface MeditationUIState {
+    object Start : MeditationUIState
+    object Loading : MeditationUIState
+    data class Success(val meditations: List<MeditationWrapper.MeditationData>) : MeditationUIState
+    data class Error(val message: String) : MeditationUIState
 }
